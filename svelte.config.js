be72f0d24a6bf5e10e9b57cb5902e5a50e2444fc
Daksh Prajapati <prajapdh@mail.uc.edu>
2024-10-23 00:26:13 -0400

    MOdify for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-// svelte.config.js
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-vercel';
 
-const config = {
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    // any additional kit configuration
-  }
+    adapter: adapter(),
+  },
 };
-
-export default config;
